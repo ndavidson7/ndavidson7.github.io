9bbd5bf11d2c14c4ec67157b803f91a2a6032d25
Nicholas Davidson <58572047+ndavidson7@users.noreply.github.com>
2026-05-28 19:21:42 -0400

    Add GTag (#11)

--- astro.config.ts
+++ astro.config.ts
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
 	site: 'https://ndavidson7.github.io',
+	integrations: [partytown()],
 });
